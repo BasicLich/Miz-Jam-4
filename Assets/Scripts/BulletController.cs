51aab159f50011a80e80e27ec1141392e9f07d8a
William Junior <williamr.alvesjr@gmail.com>
2020-08-14 22:08:41 -0300

    Add shooting to player

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class BulletController : MonoBehaviour
+{
+    private const float Speed = 7;
+
+    private const float FuseLength = 0.8f;
+    private float _destructTime = 0.0f;
+    
+    private Rigidbody2D _rigidbody2D;
+
+    public Vector2 direction;
+    
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _destructTime = Time.time + FuseLength;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = direction * Speed;
+        
+        if(_destructTime < Time.time)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private float _nextAllowedShootTime = 0.0f;
+    private float cooldownTime = 0.5f;
+    
+    private void Update()
+    {
+        if(_nextAllowedShootTime == 0.0f || _nextAllowedShootTime < Time.time)
+        {
+
+            if (Input.GetKey(KeyCode.L))
+            {
+                bulletPrefab.transform.position = transform.position;
+                var bullet = Instantiate(bulletPrefab);
+                bullet.GetComponent<BulletController>().direction = Vector2.right;
+                _nextAllowedShootTime = Time.time + cooldownTime;
+            } 
+            else if (Input.GetKey(KeyCode.J))
+            {
+                bulletPrefab.transform.position = transform.position;
+                var bullet = Instantiate(bulletPrefab);
+                bullet.GetComponent<BulletController>().direction = Vector2.left;
+                _nextAllowedShootTime = Time.time + cooldownTime;
+            }
+            else if (Input.GetKey(KeyCode.I))
+            {
+                bulletPrefab.transform.position = transform.position;
+                var bullet = Instantiate(bulletPrefab);
+                bullet.GetComponent<BulletController>().direction = Vector2.up;
+                _nextAllowedShootTime = Time.time + cooldownTime;
+            }
+            else if (Input.GetKey(KeyCode.K))
+            {
+                bulletPrefab.transform.position = transform.position;
+                var bullet = Instantiate(bulletPrefab);
+                bullet.GetComponent<BulletController>().direction = Vector2.down;
+                _nextAllowedShootTime = Time.time + cooldownTime;
+            }
+        }
+    }
+}
